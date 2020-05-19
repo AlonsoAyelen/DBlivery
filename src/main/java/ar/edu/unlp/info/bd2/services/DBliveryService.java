5bac0d6b646c5c7739f6ea4029abee09f5856703
Ayelen <ayee.alonsoo@gmail.com>
2020-05-19 14:15:22 -0300

    nueva rama con mongo lista para arrancar

--- src/main/java/ar/edu/unlp/info/bd2/services/DBliveryService.java
+++ src/main/java/ar/edu/unlp/info/bd2/services/DBliveryService.java
@@ -5,9 +5,9 @@ import ar.edu.unlp.info.bd2.repositories.DBliveryException;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import org.bson.types.ObjectId;
 
-
-public interface DBliveryService extends DBliveryStatisticsService{
+public interface DBliveryService {
     
 	/**
 	 *  Crea y devuelve un nuevo Producto.
@@ -50,14 +50,16 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return el producto modificado
 	 * @throws DBliveryException en caso de que no exista el producto para el id dado
 	 */
-	Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException;
+	Product updateProductPrice(ObjectId id, Float price, Date startDate) throws DBliveryException;
+
+
 	
 	/**
 	 * Obtiene el usuario por id
 	 * @param id
 	 * @return el usuario con el id provisto
 	 */
-	Optional<User> getUserById(Long id);
+	Optional<User> getUserById(ObjectId id);
 	
 	/**
 	 * Obtiene el usuario por el email
@@ -73,19 +75,19 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 */
 	Optional<User> getUserByUsername(String username);
 	
-	/**
-	 * Obtiene el producto por id
-	 * @param id 
-	 * @return el producto con el id provisto
-	 */
-	Optional<Product> getProductById(Long id);
-	
+//	/**
+//	 * Obtiene el producto por id
+//	 * @param id 
+//	 * @return el producto con el id provisto
+//	 */
+//	Optional<Product> getProductById(Long id);
+//	
 	/**
 	 * Obtiene el pedido por id
 	 * @param id
 	 * @return el pedido con el id provisto
 	 */
-	Optional<Order> getOrderById(Long id);
+	Optional<Order> getOrderById(ObjectId id);
 	
 	/**
 	 * Crea y retorna un nuevo pedido
@@ -106,7 +108,7 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return el pedido con el nuevo producto
 	 * @throws DBliveryException en caso de no existir el pedido
 	 */
-	Order addProduct (Long order,Long quantity, Product product )throws DBliveryException;
+	Order addProduct (ObjectId order,Long quantity, Product product )throws DBliveryException;
 
 	/**
 	 * Registra el envío del pedido, registrando al repartidor y cambiando su estado a Send.
@@ -115,8 +117,9 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return el pedido modificado
 	 * @throws DBliveryException en caso de no existir el pedido, que el pedido no se encuentre en estado Pending o sí no contiene productos.
 	 */
-	Order deliverOrder(Long order, User deliveryUser) throws DBliveryException;
-	Order deliverOrder(Long order, User deliveryUser, Date date) throws DBliveryException;
+
+	Order deliverOrder(ObjectId order, User deliveryUser) throws DBliveryException;
+	Order deliverOrder(ObjectId order, User deliveryUser, Date date) throws DBliveryException;
 
 	/**
 	 * Cancela un pedido
@@ -124,8 +127,8 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return el pedido modificado
 	 * @throws DBliveryException en caso de no existir el pedido o si el pedido no esta en estado pending
 	 */
-	Order cancelOrder(Long order) throws DBliveryException;
-	Order cancelOrder(Long order, Date date) throws DBliveryException;
+	Order cancelOrder(ObjectId order) throws DBliveryException;
+	Order cancelOrder(ObjectId order, Date date) throws DBliveryException;
 
 	/**
 	 * Registra la entrega de un pedido.
@@ -133,8 +136,8 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return el pedido modificado
 	 * @throws DBliveryException en caso que no exista el pedido o si el mismo no esta en estado Send
 	 */
-	Order finishOrder(Long order) throws DBliveryException;
-	Order finishOrder(Long order, Date date) throws DBliveryException;
+	Order finishOrder(ObjectId order) throws DBliveryException;
+	Order finishOrder(ObjectId order, Date date) throws DBliveryException;
 	
 	/**
 	 * verifica si un pedido se puede cancelar, para lo cual debe estar en estado pending
@@ -142,7 +145,7 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return true en caso que pueda ser cancelado false en caso contrario.
 	 * @throws DBliveryException si no existe el pedido.
 	 */
-	boolean canCancel(Long order) throws DBliveryException;
+	boolean canCancel(ObjectId order) throws DBliveryException;
 
 	/**
 	 * verifica si se puede finalizar un pedido
@@ -150,28 +153,27 @@ public interface DBliveryService extends DBliveryStatisticsService{
 	 * @return true en caso que pueda ser finalizado, false en caso contrario
 	 * @throws DBliveryException en caso de no existir el pedido
 	 */
-	boolean canFinish(Long id) throws DBliveryException;
-	
+	boolean canFinish(ObjectId id) throws DBliveryException;	
 	/**
 	 * verifica si un pedido puede ser enviado para lo cual debe tener productos y estar en estado pending
 	 * @param order pedido a ser enviado
 	 * @return true en caso que pueda ser enviado, false en caso contrario
 	 * @throws DBliveryException si el pedido no esta en estado pending.
 	 */
-	boolean canDeliver(Long order) throws DBliveryException;
+	boolean canDeliver(ObjectId order) throws DBliveryException;
 	
 	/**
 	 * Obtiene el estado actual de un pedido.
 	 * @param order pedido del cual se debe retornar el estado actual
 	 * @return el estado del pedido actual
 	 */
-	OrderStatus getActualStatus(Long order);
+	OrderStatus getActualStatus(ObjectId order);
 	
 	/**
 	 * Obtiene el listado de productos que su nombre contega el string dado
 	 * @param name string a buscar
 	 * @return Lista de productos
 	 */
-	List<Product> getProductByName(String name);
+	List<Product> getProductsByName(String name);
 
 }
