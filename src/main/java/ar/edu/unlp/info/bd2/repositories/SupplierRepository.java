efc4173f4f28e54be55dfe96f9da373ef2b8c71a
Federico <dartist@Federicos-MacBook-Air.local>
2020-07-05 21:59:51 -0300

    removed unnecessary methods and legacy code/comments

--- src/main/java/ar/edu/unlp/info/bd2/repositories/SupplierRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/SupplierRepository.java
@@ -8,5 +8,4 @@ import ar.edu.unlp.info.bd2.model.Supplier;
 
 public interface SupplierRepository extends CrudRepository<Supplier,Long> {
 
-	public Optional<Supplier> getById(Long id);
 }
--- src/main/java/ar/edu/unlp/info/bd2/repositories/UserRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/UserRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import ar.edu.unlp.info.bd2.model.User;
 
 public interface UserRepository extends CrudRepository<User,Long> {
-
-	public Optional<User> getById(Long id);
 	public Optional<User> getByUsername(String username);
 	public Optional<User> getByEmail(String email);
 }
